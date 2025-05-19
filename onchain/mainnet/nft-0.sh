6b37d5a997718b69f1fcf8edf2c601a9867f1614
Brian W Bush <code@functionally.io>
2025-05-19 08:26:15 -0600

    Adjusted turtle

--- onchain/mainnet/nft-0.sh
+++ onchain/mainnet/nft-0.sh
@@ -1,15 +1,14 @@
 
 export PATH=/extra/iohk/bin:$PATH
-export CARDANO_NODE_SOCKET_PATH=/extra/iohk/networks/preprod/node.socket
-export CARDANO_NODE_NETWORK_ID=1
-NETWORK=testnet
+export CARDANO_NODE_SOCKET_PATH=/extra/iohk/networks/mainnet/node.socket
+export CARDANO_NODE_NETWORK_ID=mainnet
+NETWORK=mainnet
 
 cardano-cli conway query utxo --address $(cat nacatgunma.$NETWORK.address)
 
 cardano-cli conway transaction build \
-  --tx-in 1e6a020537ffc34b9e10cb8c4eab9c8cc95f5bbf6eb94ae2648c01c0a32e58d8#0 \
-  --tx-in 82683878a41adcf19330e025b1fe08f342820ac487dc017b3a02c17951dcd630#0 \
-  --tx-in ae1fa36e2f82b9d031169b8aaa2f139dcd62d93820122c3ff37cd0ee4386ef64#0 \
+  --tx-in 069bc8f72c074c9c4dc2b25a0fcdc0158a88260a37b63a9f16beca04ff6e227f#1 \
+  --tx-in ad4f9d12f8f9ffd80e5352c1d40137052ca0ee6e608c1eea645346b1e616d12f#0 \
   --tx-out "$(cat nacatgunma.$NETWORK.address)+1500000+1 $(cat controllers.hash).4e6163617467756e6d61" \
   --change-address $(cat nacatgunma.$NETWORK.address) \
   --mint "1 $(cat controllers.hash).4e6163617467756e6d61" \
